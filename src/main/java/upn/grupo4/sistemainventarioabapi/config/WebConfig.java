c8590546c923dbbc4652066ce89caf14ccedd3d9
ginoalejandro <ginoalejandro2304@gmail.com>
2025-10-19 19:47:31 -0500

    ADD implementacion configuracion de cors

--- src/main/java/upn/grupo4/sistemainventarioabapi/config/WebConfig.java
+++ src/main/java/upn/grupo4/sistemainventarioabapi/config/WebConfig.java
@@ -1,4 +1,17 @@
 package upn.grupo4.sistemainventarioabapi.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
 }
